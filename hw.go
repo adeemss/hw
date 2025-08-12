b5734c83d49b98c800dc97aa5d59d838edc02674
adeemss <bakhytova.adeem@gmail.com>
2025-08-12 12:45:30 +0500

    Update hw.go

--- hw.go
+++ hw.go
@@ -1,133 +1,63 @@
-package main
-
-import (
-	"fmt"
-	"time"
-)
-
-func main() {
-	fmt.Println("hw#1")
-	c := make(chan any)
-
-	cOut := doBatching(c, 2)
-
-	go func() {
-		for i := 1; i <= 10; i++ {
-			c <- i
-			fmt.Println("vvela: ", i)
-			time.Sleep(300 * time.Millisecond)
-		}
-		close(c)
-
-	}()
-	for i := range cOut {
-		fmt.Println(i) // [1,2] [3,4]
-	}
-
-
-	
-	d := make(chan any)
-	dOut := doB(d, 3)
-	go func() {
-		for i := 1; i <= 10; i++ {
-			d <- i
-			fmt.Println("vvela: ", i)
-			time.Sleep(300 * time.Millisecond)
-		}
-		close(d)
-
-	}()
-	for di := range dOut {
-		fmt.Println("dddd", di)
-	}
-	
-}
-func doBatching(c chan any, batchSize int) chan []any {
-	resp := make(chan []any)
-	go func() {
-		defer close(resp)
-		batch := make([]any, 0, batchSize)
-
-		for val := range c {
-			batch = append(batch, val)
-			if len(batch) > batchSize {
-				resp <- batch
-				batch = make([]any, 0, batchSize)
-			}
-		}
-		if len(batch) > 0 {
-			resp <- batch
-		}
-	}()
-	return resp
-}
-// case just for me w select
-func doB(d chan any, batchSize int) chan []any {
-	resp := make(chan []any)
-	var batch []any
-	go func() {
-		defer close(resp)
-		for {
-			select {
-			case val, ok := <-d:
-				if !ok {
-					if len(batch) > 0 {
-						resp <- batch
-					}
-					return
-				}
-				if len(batch) == batchSize {
-					resp <- batch
-					batch = nil
-				}
-				batch = append(batch, val)
-			}
-		}
-	}()
-	return resp
-}
-// case w WaitGroup 
-/*func main() {
-	c := make(chan any)
-	d := make(chan any)
-
-	cOut := doBatching(c, 2)
-	dOut := doB(d, 2)
-
-	var wg sync.WaitGroup
-	wg.Add(4)
-
-	go func() {
-		defer wg.Done()
-		for i := 1; i <= 10; i++ {
-			c <- i
-			fmt.Println("c channel is ", i)
-			time.Sleep(1 * time.Second)
-		}
-		close(c)
-	}()
-	go func() {
-		defer wg.Done()
-		for i := 1; i <= 10; i++ {
-			d <- i
-			fmt.Println("d channel is ", i)
-			time.Sleep(1 * time.Second)
-		}
-		close(d)
-	}()
-
-	go func() {
-		defer wg.Done()
-		for i := range cOut {
-			fmt.Println("c batch ", i)
-		}
-	}()
-	go func() {
-		defer wg.Done()
-		for i := range dOut {
-			fmt.Println("d batch ", i)
-		}
-	}()
-	wg.Wait()
-
-}*/
+/v1/resources/extSystemId={extSystemId}:
+  get:
+    summary: Получение списка ресурсов по идентификатору внешней системы
+    operationId: GetResourcesByExtSystemId
+    parameters:
+      - name: extSystemId
+        in: path
+        description: Уникальный идентификатор внешней системы
+        required: true
+        schema:
+          type: integer
+    tags:
+      - Resource
+    responses:
+      '200':
+        description: Список ресурсов внешней системы по айди
+        content:
+          application/json:
+            schema:
+              type: object
+              description: Список ресурсов внешней системы по айди
+              required:
+                - resources
+              properties:
+                resources:
+                  type: array
+                  items:
+                    $ref: '#/components/schemas/ExtSystemResource'
+            examples:
+              success:
+                summary: Успешный пример
+                value:
+                  resources:
+                    - id: 501
+                      code: GET_CUSTOMER_PROFILE
+                      name: Получение профиля клиента
+                      method: GET
+                      path: /api/customer/profile
+                      priceType: FIXED
+                      amount: 100.00
+                      extSystemId: 301
+                      cacheStoragePeriod: 7
+                      active: true
+                      createdAt: "2025-08-01T11:47:58+05:00"
+                      updatedAt: "2025-08-01T11:47:58+05:00"
+                    - id: 502
+                      code: CREATE_LOAN_REQUEST
+                      name: Создание заявки на кредит
+                      method: POST
+                      path: /api/loan/create
+                      priceType: PER_REQUEST
+                      amount: 250.00
+                      extSystemId: 301
+                      cacheStoragePeriod: 0
+                      active: false
+                      createdAt: "2025-07-28T09:30:00+05:00"
+                      updatedAt: "2025-07-30T12:15:00+05:00"
+      '400':
+        $ref: '#/components/responses/BadRequest'
+      '401':
+        $ref: '#/components/responses/Unauthorized'
+      '500':
+        $ref: '#/components/responses/InternalError'
