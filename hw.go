8139b46398bfdf1982c51e2c87c31d62577b7035
adeemss <bakhytova.adeem@gmail.com>
2025-09-25 11:51:14 +0500

    Update hw.go

--- hw.go
+++ hw.go
@@ -1,46 +1,15 @@
-type client struct {
-	cl                 *redis.ClusterClient
-	InitSystemsDataKey string
-	AttributesDataKey  string
-	ExtSystemsDataKey  string
-	ResponseKeyPrefix  string
-}
-
-func NewClient(ctx context.Context, cfg config.Config) (*client, error) {
-	addrs := strings.Split(cfg.Redis.Addresses, ";")
-	cli := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs:    addrs,
-		Password: cfg.Redis.Password,
-	})
-
-	if err := cli.Ping(ctx).Err(); err != nil {
-		return nil, fmt.Errorf("failed to connect to redis server: %w", err)
+func (c client) SetInitSystems(ctx context.Context, initSystems []blDTO.InitSystem) error {
+	initSysVals := make(map[string]uint)
+	for _, initSystem := range initSystems {
+		converter.ParseInitSystem(initSystem, initSysVals)
 	}
 
-	return &client{
-		cl:                 cli,
-		InitSystemsDataKey: InitSystemDataKey(cfg.Redis.KeyPrefix),
-		AttributesDataKey:  AttributeDataKey(cfg.Redis.KeyPrefix),
-		ExtSystemsDataKey:  ExtSystemsDataKey(cfg.Redis.KeyPrefix),
-		ResponseKeyPrefix:  cfg.Redis.KeyPrefix,
-	}, nil
-}
-
-func (c client) Close() error {
-	if err := c.cl.Close(); err != nil {
-		return fmt.Errorf("failed to close redis connection, %w", err)
+	value, err := json.Marshal(initSysVals)
+	if err != nil {
+		return fmt.Errorf("failed to prepare for set value, %w", err)
+	}
+	if err := c.cl.Set(ctx, c.InitSystemsDataKey, value, 0).Err(); err != nil {
+		return fmt.Errorf("failed to set InitSystems directory, %w", err)
 	}
 	return nil
-}
-
-func (c client) GetInitSystems(ctx context.Context) (map[string]uint, error) {
-	var res map[string]uint
-	value, err := c.cl.Get(ctx, c.InitSystemsDataKey).Result()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get value by key, %w", err)
-	}
-	if err := json.Unmarshal([]byte(value), &res); err != nil {
-		return nil, fmt.Errorf("failed to prepare received value, %w", err)
-	}
-	return res, nil
 }
