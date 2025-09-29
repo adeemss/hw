c7ec109a0ba11e25000563eaf9346512a871bb07
adeemss <bakhytova.adeem@gmail.com>
2025-09-29 12:57:49 +0500

    Update hw.go

--- hw.go
+++ hw.go
@@ -1,12 +1,96 @@
-GOROOT=C:\Users\bakhyade\go\go1.23.5 #gosetup
-GOPATH=C:\Users\bakhyade\go #gosetup
-C:\Users\bakhyade\go\go1.23.5\bin\go.exe test -c -o C:\Users\bakhyade\AppData\Local\JetBrains\GoLand2025.1\tmp\GoLand\___1TestSaveBody_in_gitlab_bcc_kz_digital_banking_platform_microservices_billing_dbp_ext_requests_billing_system_server_store_client_minio.test.exe gitlab.bcc.kz/digital-banking-platform/microservices/billing/dbp-ext-requests-billing-system/server/store/client/minio #gosetup
-# gitlab.bcc.kz/digital-banking-platform/microservices/billing/dbp-ext-requests-billing-system/server/store/client/minio
-FAIL	gitlab.bcc.kz/digital-banking-platform/microservices/billing/dbp-ext-requests-billing-system/server/store/client/minio [setup failed]
-package gitlab.bcc.kz/digital-banking-platform/microservices/billing/dbp-ext-requests-billing-system/server/store/client/minio
-FAIL
-	imports gitlab.bcc.kz/digital-banking-platform/microservices/billing/dbp-ext-requests-billing-system/server/store/client/minio/mock
-	imports gitlab.bcc.kz/digital-banking-platform/microservices/billing/dbp-ext-requests-billing-system/server/store/client/minio: import cycle not allowed in test
+package minio
 
-Compilation finished with exit code 1
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"github.com/minio/minio-go/v7"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	min "gitlab.bcc.kz/digital-banking-platform/microservices/billing/dbp-ext-requests-billing-system/server/store/client/minio/mock"
+	"strconv"
+	"testing"
+)
 
+func TestSaveBody(t *testing.T) {
+	inpuFileName := "testdata/billing.json"
+	ctx := context.Background()
+	bucket := "bucket"
+	retention := "retention"
+
+	body := "test-body"
+	pl := Payload{
+		Body: body,
+	}
+
+	result := minio.UploadInfo{
+		Bucket: bucket,
+	}
+	testCases := map[string]struct {
+		inputFileName       string
+		inputRetenionPeriod int
+		MinioSetup          func(t *testing.T, bs *min.MinIOClientServicesMock)
+		ExpErr              error
+	}{
+		"success": {
+			inputFileName:       inpuFileName,
+			inputRetenionPeriod: 10,
+			MinioSetup: func(t *testing.T, bs *min.MinIOClientServicesMock) {
+				expJson, _ := json.Marshal(pl)
+				reader := bytes.NewReader(expJson)
+				bs.On("PutObject", ctx,
+					mock.MatchedBy(func(bucketName string) bool {
+						return bucketName == bucket
+					}), mock.MatchedBy(func(FileName string) bool {
+						return inpuFileName == FileName
+					}),
+					reader,
+					int64(len(expJson)),
+					minio.PutObjectOptions{ContentType: "application/json", UserTags: map[string]string{
+						retention: strconv.Itoa(10),
+					}}).Return(result, nil)
+			},
+		},
+		"failed": {
+			inputFileName:       inpuFileName,
+			inputRetenionPeriod: 10,
+			MinioSetup: func(t *testing.T, bs *min.MinIOClientServicesMock) {
+				expJson, _ := json.Marshal(pl)
+				reader := bytes.NewReader(expJson)
+				bs.On("PutObject", ctx,
+					mock.MatchedBy(func(bucketName string) bool {
+						return bucketName == bucket
+					}), mock.MatchedBy(func(FileName string) bool {
+						return inpuFileName == FileName
+					}),
+					reader,
+					int64(len(expJson)),
+					minio.PutObjectOptions{ContentType: "application/json", UserTags: map[string]string{
+						retention: strconv.Itoa(10),
+					}}).Return(minio.UploadInfo{}, assert.AnError)
+			},
+			ExpErr: assert.AnError,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			mockMinio := min.NewMinIOClientServicesMock(t)
+			client := client{
+				cl:           mockMinio,
+				bucketName:   bucket,
+				retentionTag: retention,
+			}
+			tc.MinioSetup(t, mockMinio)
+			err := client.SaveBody(ctx, tc.inputFileName, body, tc.inputRetenionPeriod)
+			if tc.ExpErr != nil {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			mockMinio.AssertExpectations(t)
+		})
+	}
+}
